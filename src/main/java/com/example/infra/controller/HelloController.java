142b38c1b0ad5036048222d4051970df7bb8c193
han51361 <han51361@gmail.com>
2022-06-15 23:14:27 +0900

    k8s yaml

--- src/main/java/com/example/infra/controller/HelloController.java
+++ src/main/java/com/example/infra/controller/HelloController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @RequestMapping("/hello-world")
     public String hello(){
         return "hello, world";
     }
 }
+
+// 이미지를 만들어야 하는데 일반적으로 도커 파일 만들어서 빌드해서 사용하는데
+// jib는 gitOps -> 이미지 만들어서 푸시 (이미지 생성시 이 앱 말고 다른 작업을 안할 거면 용이하다)
+// https://github.com/GoogleContainerTools/jib
